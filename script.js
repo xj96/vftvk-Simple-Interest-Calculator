d3b83d76973e94aaef97b84e7f05099d62b50638
Xavier Jackson <Xavier@xavier-jackson.fios-router.home>
2022-02-27 15:35:18 -0500

    updated script.js file

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
-function compute()
-{
-    p = document.getElementById("principal").value;
-    
+function compute() {
+    const principal = document.getElementById("principal").value;
+    const rate = document.getElementById("rate").value;
+    const years = document.getElementById("years").value;
+    const interest = principal * years * rate / 100;
+    const year = new Date().getFullYear()+parseInt(years);
+}
+
+function updateRate() {
+    const rateVal = document.getElementById("rate").value;
+    document.getElementById("rateval").innerText=rateVal
 }
         
