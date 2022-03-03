63bdb2155e9c0842397907c243e4b88663718b18
Xavier Jackson <Xavier@xavier-jackson.fios-router.home>
2022-03-02 21:55:39 -0500

    removed double id issue on index.html

--- script.js
+++ script.js
@@ -1,13 +1,23 @@
-function compute() {
+const calculateInterest = () => {
     const principal = document.getElementById("principal").value;
-    const rate = document.getElementById("rate").value;
+    const interest = document.getElementById("interest").value;
     const years = document.getElementById("years").value;
-    const interest = principal * years * rate / 100;
-    const year = new Date().getFullYear()+parseInt(years);
+    const interestGain = principal * years * interest / 100;
+    const year = new Date().getFullYear() + parseInt(years);
+    const result = document.getElementById("result").innerHTML =
+        `If you deposit <span style="background-color:yellow;">${principal}</span><br/>
+        at an interest rate of <span style="background-color:yellow;">${interest}</span><br/>
+        you will receive an amount of <span style="background-color:yellow;">${interestGain}</span><br/>
+        in the year <span style="background-color:yellow;">${year}</span>`;
+
+    if (principal == 0 || principal < 0) {
+        alert("Please enter a positive number!");
+        focus(principal);
+    }
 }
 
-function updateRate() {
-    const rateVal = document.getElementById("rate").value;
-    document.getElementById("rateval").innerText=rateVal
+const updateRate = () => {
+    const interestRate = document.getElementById("interest").value;
+    document.getElementById("interest_val").innerHTML = `${interestRate}%`;
+    console.log(interestRate);
 }
-        
