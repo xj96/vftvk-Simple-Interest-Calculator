e09e590958311606426a874aafa9731030e94a77
Xavier Jackson <Xavier@xavier-jackson.fios-router.home>
2022-03-02 22:43:23 -0500

    created if-else statement for form validation

--- script.js
+++ script.js
@@ -4,15 +4,18 @@ const calculateInterest = () => {
     const years = document.getElementById("years").value;
     const interestGain = principal * years * interest / 100;
     const year = new Date().getFullYear() + parseInt(years);
-    const result = document.getElementById("result").innerHTML =
-        `If you deposit <span style="background-color:yellow;">${principal}</span><br/>
-        at an interest rate of <span style="background-color:yellow;">${interest}</span><br/>
-        you will receive an amount of <span style="background-color:yellow;">${interestGain}</span><br/>
-        in the year <span style="background-color:yellow;">${year}</span>`;
+
 
     if (principal == 0 || principal < 0) {
         alert("Please enter a positive number!");
         document.getElementById("principal").focus();
+    }
+    else {
+        const result = document.getElementById("result").innerHTML =
+            `If you deposit <span style="background-color:yellow;">${principal}</span><br/>
+            at an interest rate of <span style="background-color:yellow;">${interest}</span><br/>
+            you will receive an amount of <span style="background-color:yellow;">${interestGain}</span><br/>
+            in the year <span style="background-color:yellow;">${year}</span>`;
     }
 }
 
